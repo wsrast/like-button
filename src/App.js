96ec3713197fa8e893f10f6d0cb1fe65ffdcbaaa
rastwe <wes.rast@ey.com>
2018-11-20 14:39:29 -0500

    Replace app css with SC.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import Like from './Like';
+import styled from 'styled-components';
 
-import './App.css';
+const AppStyled = styled.div`
+  text-align: center;
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <AppStyled className="App">
 	      <Like />
-      </div>
+      </AppStyled>
     );
   }
 }
