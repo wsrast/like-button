3394365a907f5b8b470c05b294ee5486d2101ca6
rastwe <wes.rast@ey.com>
2018-11-20 11:44:35 -0500

    Separate and import icon assets. Remove old img based svg

--- src/Like/LikeEmoji/index.js
+++ src/Like/LikeEmoji/index.js
@@ -1,17 +1,23 @@
 import React, {Component} from 'react';
 import './index.css';
+import {ReactComponent as LikeSVG} from '../../assets/like.svg';
+import {ReactComponent as LoveSVG} from '../../assets/love.svg';
+import {ReactComponent as HahaSVG} from '../../assets/haha.svg';
+import {ReactComponent as WowSVG} from '../../assets/wow.svg';
+import {ReactComponent as SadSVG} from '../../assets/sad.svg';
+import {ReactComponent as AngrySVG} from '../../assets/angry.svg';
 
 export default class LikeEmoji extends Component {
 
 	render() {
 		return (
 			<div className="like-emoji">
-				<span className="emoji like">like</span>
-				<span className="emoji love">love</span>
-				<span className="emoji haha">haha</span>
-				<span className="emoji wow">wow</span>
-				<span className="emoji sad">sad</span>
-				<span className="emoji angry">angry</span>
+				<LikeSVG className="emoji"/>
+				<LoveSVG className="emoji"/>
+				<HahaSVG className="emoji"/>
+				<WowSVG className="emoji"/>
+				<SadSVG className="emoji"/>
+				<AngrySVG className="emoji"/>
 			</div>
 		)
 	}
